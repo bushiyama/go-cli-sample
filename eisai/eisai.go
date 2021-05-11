58d02c3f6c3fbed9002e05b972816d1945ce66f4
bushiyama <45021814+bushiyama@users.noreply.github.com>
2021-05-11 14:28:54 +0900

    Update eisai.go

--- eisai/eisai.go
+++ eisai/eisai.go
@@ -10,11 +10,27 @@ import (
 
 const version = "1.0.0"
 
+var (
+	flag1 bool
+)
+
 func main() {
 	app := &cli.App{
-		Usage: "If you type eisai, haramascoi will be returned.",
+		Usage: "If you type eisai, haramascoi will be returned. option \"-p\"",
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:        "p",
+				Usage:       "hayaranai",
+				Destination: &flag1,
+				Required:    false,
+			},
+		},
 		Action: func(c *cli.Context) error {
-			fmt.Println("haramasuko-i")
+			ret := "haramasuko-i"
+			if flag1 {
+				ret = "e-nn"
+			}
+			fmt.Println(ret)
 			return nil
 		},
 	}
